4be298318889f60dafe8149b48ba6886c313befe
Alan <Alan2003@sju.ac.kr>
2024-07-14 00:16:37 +0900

    ,,

--- script.js
+++ script.js
@@ -132,3 +132,23 @@ document.addEventListener('keydown', function (e) {
 // logo.classList.toggle('c'); //adding class
 // console.log(logo.classList.contains('c')); //true
 // console.log(logo);
+//
+// rgb(255, 255, 255);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelecto('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('-nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelecto('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
